b4e99876f1d6081d2adedba1549a708602f51fc6
nate <nate@Nathan-Laptop>
2017-04-13 09:51:02 -0400

    finished hangman

--- src/assignment5/Assignment5.java
+++ src/assignment5/Assignment5.java
@@ -17,19 +17,22 @@ public class Assignment5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+//ceates the correct variable
         int correct = 0;
-
+//creates a new scanner
         Scanner input = new Scanner(System.in);
 
         //player 1 enters in the word 
         System.out.println("please enter in a word for player 2 to guess");
+        //get word
         String word = input.nextLine();
+        //turnes the word into lower case
         word = word.toLowerCase();
         //clear the screen
         for (int i = 0; i < 99; i++) {
             System.out.println("");
         }
+        //storeing the original word
         String word2 = word;
         //show how long the word is useing dashes
         int legnth = word.length();
@@ -44,33 +47,60 @@ public class Assignment5 {
         StringBuilder sb = new StringBuilder(word);
         int lives = 5;
         //making guesses
-        while (lives > 0) {
+        while (lives > 0 && correct < legnth) {
+            //telling the player lives and further instructions + the current guessed word
             System.out.println("you have " + lives + " lives left");
-            System.out.println(sb);
-            System.out.println("player 2 make your guess");
-
+            System.out.print(sb + "   ");
+            System.out.print("player 2 please enter in a letter that you have not entered in before ");
+            System.out.println("");
+//gets guess
             String guess = input.nextLine();
-            char guessl = guess.charAt(0);
+            //turnes it into lower case to keep it the same as the original word 
+            String g = guess.toLowerCase();
+
+            //turnes the first letter enterred into a char
+            char guessl = g.charAt(0);
 
             //checks each letter for the selected letter
-            for (int i = 0; i < legnth; i++) {
-
-                char selectedL = word2.charAt(i);
-
+            int u = 0;
+            while (u < legnth) {
+//gets the letter at u
+                char selectedL = word2.charAt(u);
+//if it is the same as the guessed letter replace it in the word converted into dashes
                 if (selectedL == guessl) {
-                    sb.setCharAt(i, guessl);
+                    sb.setCharAt(u, guessl);
                     correct++;
                 }
 
+                u++;
             }
-           
-            System.out.println("type out a diferent letter");
-            if (correct == legnth) {
-                System.out.println("You have guesed the word corectly");
-                System.out.println("Payer 1 has lost");
-                System.out.println("Player 2 wins");
-                break;
+//if u reaches the end of the word decrese lives counter
+            if (u == legnth && !word2.contains(guess)) {
+                lives--;
             }
+        }
+//when the correct counter is the legnth of the word check if they used the same letter twice
+        if (correct == legnth) {
+            for (int i = 0; i < legnth;) {
+
+                char selectedL = sb.charAt(i);
+
+                if (selectedL != '-') {
+                    i++;
+                    if (i == legnth) {
+
+                        System.out.println("You have guesed the word corectly");
+                        System.out.println("Payer 1 has lost");
+                        System.out.println("Player 2 wins");
+                        break;
+                    }
+if (selectedL == '-') {
+                    System.out.println("you cheated you lose");
+                }
+                }
+                
+            }
+
         }
 
         if (lives == 0) {
